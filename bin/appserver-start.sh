e16341afbec1a91b82385c191c77c21a1902d5ed
worgflores <worgflores@gmail.com>
2021-08-12 15:45:59 +0800

    bin: updated

--- bin/appserver-start.sh
+++ bin/appserver-start.sh
@@ -5,6 +5,6 @@ BASE_DIR=`pwd`
 
 cd $BASE_DIR/appserver/epayment && docker-compose up -d
 
-cd $BASE_DIR/appserver/etracs && docker-compose up -d
+cd $BASE_DIR/appserver/etracs && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
 cd $RUN_DIR
--- bin/start.sh
+++ bin/start.sh
@@ -9,14 +9,14 @@ cd $BASE_DIR/email/mail-primary && docker-compose up -d
 
 cd $BASE_DIR/system/notification && docker-compose up -d
 
-cd $BASE_DIR/system/download && docker-compose up -d
+cd $BASE_DIR/system/download && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
 cd $BASE_DIR/system/queue && docker-compose up -d
 
 cd $BASE_DIR/system/etracs-web && docker-compose up -d
 
 
-cd $BASE_DIR/appserver/etracs && docker-compose up -d
+cd $BASE_DIR/appserver/etracs && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
 cd $BASE_DIR/appserver/epayment && docker-compose up -d
 
