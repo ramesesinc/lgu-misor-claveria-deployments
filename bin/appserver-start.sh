fcbae2b719732568f9da97c68c7ef4a14bda2ed0
worgflores <worgflores@gmail.com>
2021-08-25 10:58:54 +0800

    epayment: updated

--- bin/appserver-start.sh
+++ bin/appserver-start.sh
@@ -3,7 +3,7 @@ RUN_DIR=`pwd`
 cd ..
 BASE_DIR=`pwd`
 
-cd $BASE_DIR/appserver/epayment && docker-compose up -d
+cd $BASE_DIR/appserver/epayment && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
 cd $BASE_DIR/appserver/etracs && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
