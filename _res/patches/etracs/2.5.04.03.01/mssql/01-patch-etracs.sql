b47841ac4dfa7eb81189de83a1354ecc32fd7efa
worgflores <worgflores@gmail.com>
2021-09-27 22:10:30 +0800

    _res: patches: updated

--- _res/patches/etracs/2.5.04.03.01/mssql/01-patch-etracs.sql
+++ _res/patches/etracs/2.5.04.03.01/mssql/01-patch-etracs.sql
@@ -64,3 +64,95 @@ VALUES ('CASHBOOK_CERTIFIED_BY_NAME', NULL, 'Cashbook Report Certified By Name',
 
 INSERT INTO sys_var (name, value, description, datatype, category) 
 VALUES ('CASHBOOK_CERTIFIED_BY_TITLE', NULL, 'Cashbook Report Certified By Title', 'text', 'REPORT');
+
+
+
+-- ## 2021-09-27
+
+if object_id('dbo.report_bpdelinquency_item', 'U') IS NOT NULL 
+  drop table dbo.report_bpdelinquency_item; 
+go 
+if object_id('dbo.report_bpdelinquency', 'U') IS NOT NULL 
+  drop table dbo.report_bpdelinquency; 
+go 
+
+
+CREATE TABLE report_bpdelinquency (
+  objid varchar(50) NOT NULL,
+  state varchar(25) NULL,
+  dtfiled datetime NULL,
+  userid varchar(50) NULL,
+  username varchar(160) NULL,
+  totalcount int NULL,
+  processedcount int NULL,
+  billdate date NULL,
+  duedate date NULL,
+  lockid varchar(50) NULL,
+  constraint pk_report_bpdelinquency PRIMARY KEY (objid)
+) 
+go 
+CREATE INDEX ix_state ON report_bpdelinquency (state)
+go
+CREATE INDEX ix_dtfiled ON report_bpdelinquency (dtfiled)
+go
+CREATE INDEX ix_userid ON report_bpdelinquency (userid)
+go
+CREATE INDEX ix_billdate ON report_bpdelinquency (billdate)
+go
+CREATE INDEX ix_duedate ON report_bpdelinquency (duedate)
+go
+
+
+CREATE TABLE report_bpdelinquency_item (
+  objid varchar(50) NOT NULL,
+  parentid varchar(50) NOT NULL,
+  applicationid varchar(50) NOT NULL,
+  tax decimal(16,2) NOT NULL DEFAULT 0.0,
+  regfee decimal(16,2) NOT NULL DEFAULT 0.0,
+  othercharge decimal(16,2) NOT NULL DEFAULT 0.0,
+  surcharge decimal(16,2) NOT NULL DEFAULT 0.0,
+  interest decimal(16,2) NOT NULL DEFAULT 0.0,
+  total decimal(16,2) NOT NULL DEFAULT 0.0,
+  duedate date NULL,
+  year int NOT NULL,
+  qtr int NOT NULL,
+  constraint pk_report_bpdelinquency_item PRIMARY KEY (objid)
+) 
+go
+CREATE INDEX ix_parentid ON report_bpdelinquency_item (parentid);
+go
+CREATE INDEX ix_applicationid ON report_bpdelinquency_item (applicationid);
+go
+CREATE INDEX ix_year ON report_bpdelinquency_item (year);
+go
+CREATE INDEX ix_qtr ON report_bpdelinquency_item (qtr);
+go
+ALTER TABLE report_bpdelinquency_item ADD CONSTRAINT fk_report_bpdelinquency_item_parentid
+  FOREIGN KEY (parentid) REFERENCES report_bpdelinquency (objid) 
+go
+
+CREATE TABLE report_bpdelinquency_app (
+  objid varchar(50) NOT NULL,
+  parentid varchar(50) NOT NULL,
+  applicationid varchar(50) NOT NULL,
+  appdate date not null,
+  appyear int not null,
+  lockid varchar(50) NULL,
+  constraint pk_report_bpdelinquency_app PRIMARY KEY (objid)
+) 
+go 
+create unique index uix_parentid_applicationid on report_bpdelinquency_app (parentid, applicationid)
+go 
+CREATE INDEX ix_parentid ON report_bpdelinquency_app (parentid);
+go
+CREATE INDEX ix_applicationid ON report_bpdelinquency_app (applicationid);
+go
+CREATE INDEX ix_appdate ON report_bpdelinquency_app (appdate);
+go
+CREATE INDEX ix_appyear ON report_bpdelinquency_app (appyear);
+go
+CREATE INDEX ix_lockid ON report_bpdelinquency_app (lockid);
+go
+ALTER TABLE report_bpdelinquency_app ADD CONSTRAINT fk_report_bpdelinquency_app_parentid
+  FOREIGN KEY (parentid) REFERENCES report_bpdelinquency (objid) 
+go 
--- _res/patches/etracs/2.5.04.03.01/mysql/01-patch-etracs.sql
+++ _res/patches/etracs/2.5.04.03.01/mysql/01-patch-etracs.sql
@@ -61,3 +61,76 @@ VALUES ('CASHBOOK_CERTIFIED_BY_NAME', NULL, 'Cashbook Report Certified By Name',
 
 INSERT INTO sys_var (name, value, description, datatype, category) 
 VALUES ('CASHBOOK_CERTIFIED_BY_TITLE', NULL, 'Cashbook Report Certified By Title', 'text', 'REPORT');
+
+
+
+
+-- ## 2021-09-27
+
+DROP TABLE report_bpdelinquency_item;
+DROP TABLE report_bpdelinquency;
+
+CREATE TABLE `report_bpdelinquency` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NULL,
+  `dtfiled` datetime NULL,
+  `userid` varchar(50) NULL,
+  `username` varchar(160) NULL,
+  `totalcount` int(255) NULL,
+  `processedcount` int(255) NULL,
+  `billdate` date NULL,
+  `duedate` date NULL,
+  `lockid` varchar(50) NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+CREATE INDEX `ix_state` ON report_bpdelinquency (`state`);
+CREATE INDEX `ix_dtfiled` ON report_bpdelinquency (`dtfiled`);
+CREATE INDEX `ix_userid` ON report_bpdelinquency (`userid`);
+CREATE INDEX `ix_billdate` ON report_bpdelinquency (`billdate`);
+CREATE INDEX `ix_duedate` ON report_bpdelinquency (`duedate`);
+
+
+CREATE TABLE `report_bpdelinquency_item` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `applicationid` varchar(50) NOT NULL,
+  `tax` decimal(16,2) NOT NULL DEFAULT '0',
+  `regfee` decimal(16,2) NOT NULL DEFAULT '0',
+  `othercharge` decimal(16,2) NOT NULL DEFAULT '0',
+  `surcharge` decimal(16,2) NOT NULL DEFAULT '0',
+  `interest` decimal(16,2) NOT NULL DEFAULT '0',
+  `total` decimal(16,2) NOT NULL DEFAULT '0',
+  `duedate` date NULL,
+  `year` int NOT NULL,
+  `qtr` int NOT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+CREATE INDEX `ix_parentid` ON report_bpdelinquency_item (`parentid`); 
+CREATE INDEX `ix_applicationid` ON report_bpdelinquency_item (`applicationid`); 
+CREATE INDEX `ix_year` ON report_bpdelinquency_item (`year`); 
+CREATE INDEX `ix_qtr` ON report_bpdelinquency_item (`qtr`); 
+ALTER TABLE report_bpdelinquency_item ADD CONSTRAINT fk_report_bpdelinquency_item_parentid
+  FOREIGN KEY (parentid) REFERENCES report_bpdelinquency (objid) 
+; 
+
+CREATE TABLE `report_bpdelinquency_app` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `applicationid` varchar(50) NOT NULL,
+  `appdate` date not null,
+  `appyear` int not null,
+  `lockid` varchar(50) NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+create unique index uix_parentid_applicationid on report_bpdelinquency_app (parentid, applicationid); 
+CREATE INDEX `ix_parentid` ON report_bpdelinquency_app (`parentid`); 
+CREATE INDEX `ix_applicationid` ON report_bpdelinquency_app (`applicationid`); 
+CREATE INDEX `ix_appdate` ON report_bpdelinquency_app (`appdate`); 
+CREATE INDEX `ix_appyear` ON report_bpdelinquency_app (`appyear`); 
+CREATE INDEX `ix_lockid` ON report_bpdelinquency_app (`lockid`); 
+ALTER TABLE report_bpdelinquency_app ADD CONSTRAINT fk_report_bpdelinquency_app_parentid
+  FOREIGN KEY (parentid) REFERENCES report_bpdelinquency (objid) 
+; 
